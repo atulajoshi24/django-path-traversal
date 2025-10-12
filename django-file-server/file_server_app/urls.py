635e72d3a442baaf848e42b1bfaa3b4d4038c4a8
atulajoshi24 <user@localhost.localdomain>
2025-10-12 18:21:27 +0530

    ssrf examples added

--- django-file-server/file_server_app/urls.py
+++ django-file-server/file_server_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import serve_file, serve_file_secure, display_user_input, display_user_input_secure
+from .views import serve_file, serve_file_secure, display_user_input, display_user_input_secure, fetch, fetch_safe
 
 urlpatterns = [
     path('serve-file', serve_file, name='serve_file'),
     path('serve-file-secure', serve_file_secure, name='serve_file_secure'),
     path('display', display_user_input, name='display'),
     path('display-secure', display_user_input_secure, name='display-secure'),
+    path('fetch', fetch, name='fetch'),
+    path('fetch-secure', fetch_safe, name='fetch-secure'),
+
 ]
--- django-file-server/file_server_app/views.py
+++ django-file-server/file_server_app/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
 from django.utils.html import escape
 import os
+import validators
+import urllib
+import json
+
 BASE_DIR = "/home/user/"
 
 def serve_file(request): 
@@ -39,3 +43,33 @@ def display_user_input_secure(request):
     username = request.GET.get('user_input', '')
     sanitised_username = escape(username)
     return HttpResponse(f'Weclome {sanitised_username}')
+
+def fetch(request):
+    print('inside fetch')
+    try:
+        url = request.GET.get('url')
+        print('url ',url)
+        with urllib.request.urlopen(url, timeout=5) as response: 
+            data = response.read()
+        return HttpResponse(data, status=200)
+    except Exception as e: 
+        return HttpResponse(e, status=404)
+
+def fetch_safe(request): 
+    try:
+        url = request.GET.get('url')
+        if not url:
+            return JsonResponse({"error": "Please provide the url"}, status=400)
+        is_url_valid = validators.url(url)
+        print("is_url_valid", is_url_valid)
+        if not is_url_valid:
+            return JsonResponse({"error": "Invalid URL supplied"}, status=403)
+        hostname = urllib.parse.urlparse(url).hostname
+        print("hostname", hostname)
+        if not hostname or hostname not in ["eczsdstfqcuazxpjznqig12uzsrz4wbeg.oast.fun"]:
+            return JsonResponse({"error": "Domain is not allowed"}, status=403)
+        with urllib.request.urlopen(url, timeout=5) as response: 
+            data = response.read()
+        return HttpResponse(data, status=200)
+    except Exception as e: 
+        return HttpResponse(e, status=404)
