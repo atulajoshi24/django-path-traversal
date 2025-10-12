fbdff7ee662b05255b0845dbc5532d4e4a26165f
atulajoshi24 <user@localhost.localdomain>
2025-10-12 14:12:43 +0530

    xss demo added

--- django-file-server/file_server_app/urls.py
+++ django-file-server/file_server_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import serve_file, serve_file_secure
+from .views import serve_file, serve_file_secure, display_user_input, display_user_input_secure
 
 urlpatterns = [
     path('serve-file', serve_file, name='serve_file'),
     path('serve-file-secure', serve_file_secure, name='serve_file_secure'),
+    path('display', display_user_input, name='display'),
+    path('display-secure', display_user_input_secure, name='display-secure'),
 ]
--- django-file-server/file_server_app/views.py
+++ django-file-server/file_server_app/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.views import View
+from django.utils.html import escape
 import os
-BASE_DIR = "E:\\CyberSecurity\wordlists"
+BASE_DIR = "/home/user/"
 
 def serve_file(request): 
     filename = request.GET.get('filename')
@@ -29,3 +30,12 @@ def serve_file_secure(request):
     else:
         print('doesn\'t starts with base dir or file not found')  # Debugging line
         return HttpResponse("File not found", status=404)
+
+def display_user_input(request): 
+    username = request.GET.get('user_input', '')
+    return HttpResponse(f'Weclome {username}')
+
+def display_user_input_secure(request): 
+    username = request.GET.get('user_input', '')
+    sanitised_username = escape(username)
+    return HttpResponse(f'Weclome {sanitised_username}')
